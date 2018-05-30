6b3ea19fd4ace4c957c5f840e9570207cd74b010
spydir <d03206642@gmail.com>
2018-05-30 09:46:04 -0700

    updated extractor and network scanner

--- cidr_extractor.py
+++ cidr_extractor.py
@@ -7,13 +7,24 @@ afrinic = 'ftp://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-latest'
 apnic = 'ftp://ftp.apnic.net/pub/stats/apnic/delegated-apnic-latest'
 lacnic = 'ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-latest'
 
-opener = urllib.FancyURLopener()
-f = opener.open(apnic)
-lines=f.readlines()
 
-for line in lines:
-        if (('ipv4' in line) & ('SG' in line)) :
-                s=line.split("|")
-                net=s[3]
-                cidr=float(s[4])
-                print "%s/%d" % (net,(32-math. log(cidr,2)))
+def queryRegistrar(countryAbbreviation, registrar):
+    opener = urllib.FancyURLopener()
+    f = opener.open(registrar)
+    lines=f.readlines()
+    netblocks = []
+    ip = ""
+    for line in lines:
+            if (('ipv4' in line) & (countryAbbreviation in line)) :
+                    s=line.split("|")
+                    net=s[3]
+                    cidr=float(s[4])
+                    ip = "%s/%d" % (net,(32-math. log(cidr,2)))
+                    netblocks.append(ip)
+
+    return netblocks
+
+def getIps(countryAbbreviation):
+    ips = queryRegistrar(countryAbbreviation, apnic)
+
+    return ips
