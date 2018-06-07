702bf658abc673182b5b2a6357354fe0a7dac13f
Niko <nikolay.akatyev@gmail.com>
2018-06-07 18:55:57 +0900

    added vscode to gitignore

--- networkScanner.py
+++ networkScanner.py
@@ -9,11 +9,13 @@ sg = getIps('SG')
 
 def scanner(ipList):
     scan = []
+    ipList = testList1
     for i in ipList:
         print i
-        proc = ZmapProcess(targets=i, options='-B 100M', probe_module='icmp_echoscan', port='80')
+        proc = ZmapProcess(targets=i, options='-B 100M', probe_module='icmp_echoscan', port='80', yield_raw=True)
         for obj in proc.run():
-            print obj.saddr
+            #print obj.saddr
+            print obj
 
 
 if __name__ == "__main__":
